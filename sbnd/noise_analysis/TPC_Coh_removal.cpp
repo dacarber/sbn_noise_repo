279fc5fd7645703df7d4b14eac4fe068e5ca5eb1
Dan Carber <whasian63@gmail.com>
2024-03-18 08:20:33 -0500

    Debugging dead channels

--- sbnd/noise_analysis/TPC_Coh_removal.cpp
+++ sbnd/noise_analysis/TPC_Coh_removal.cpp
@@ -225,6 +225,9 @@ void LoadRawDigits(TFile *inFile)
 					cout<<"2"<<endl;
 					float mid_length = get_wire_length(channel-16,wire_lengths);
 					cout<<"3"<<channel_group[kh].size()<<endl;
+					if (channel_group[kh].size() != 3415){
+						continue;
+					}
 					vector<float> int_wave = Coh_removal(channel_group[kh],coherent_waveform,kh_length,mid_length);
 					cout<<"4"<<endl;
 					float Int_RMS = Noise_levels(int_wave);
