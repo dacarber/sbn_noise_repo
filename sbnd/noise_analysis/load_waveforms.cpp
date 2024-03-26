06f0cee6d8dcc402144d339b320f7a4c3bf5ecd6
Dan Carber <whasian63@gmail.com>
2024-03-26 17:01:01 -0500

    Debugging

--- sbnd/noise_analysis/load_waveforms.cpp
+++ sbnd/noise_analysis/load_waveforms.cpp
@@ -144,11 +144,12 @@ void LoadRawDigits(TFile *inFile,int sel_evt)
 			else { 
 			for (size_t itick=0; itick < 3415; ++itick){
 				tick = myADC[in].ADC(itick);
-				cout<<tick<<" : "<<itick<<endl;
+
 				x.push_back(myADC[in].ADC(itick));
 				tree->Fill();
 
 			}
+			cout<<tick<<" : "<<itick<<endl;
 			
 			//x.clear();
 			} 
