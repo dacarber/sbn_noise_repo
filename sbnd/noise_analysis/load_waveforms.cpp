40084d077520c2aeaad8f81431aeb573e1ed8f8e
Dan Carber <whasian63@gmail.com>
2024-03-26 12:56:52 -0500

    Debugging

--- sbnd/noise_analysis/load_waveforms.cpp
+++ sbnd/noise_analysis/load_waveforms.cpp
@@ -128,8 +128,9 @@ void LoadRawDigits(TFile *inFile,int sel_evt)
 			vector<float> x;
 			int channel = myADC[ki].Channel();
 			auto index = find(channels.begin(),channels.end(), ki);
-			int in = index-channels.begin();
-			cout<<"Index:"<<in<<", Channel:"<<myADC[index].Channel()<<", Loop index:"<<ki<<endl;
+			//int in = index-channels.begin();
+			int in = index;
+			cout<<"Index:"<<in<<", Channel:"<<myADC[in].Channel()<<", Loop index:"<<ki<<endl;
 			if (myADC[in].NADC() != 3415){
 				cout<<"Dead Channel"<<endl;
 				for (size_t itick=0; itick < 3415; ++itick){
