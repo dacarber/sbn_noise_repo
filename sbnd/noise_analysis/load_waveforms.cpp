6153d41c3c160bcbe2d11536b8f3929b17c6f663
Dan Carber <whasian63@gmail.com>
2024-04-16 11:02:47 -0500

    Bug fix to make unique file names

--- sbnd/noise_analysis/load_waveforms.cpp
+++ sbnd/noise_analysis/load_waveforms.cpp
@@ -103,7 +103,7 @@ void LoadRawDigits(TFile *inFile,int sel_evt)
 			cout<<"Skip event: "<<event_num<<endl;
 			continue;
 		}
-		TString filename = "waveform_"+to_string(event_num)+".root";
+		TString filename = "waveform_"+std::to_string(event_num)+".root";
 		TFile* file = new TFile(filename, "RECREATE");
 		TTree* tree = new TTree("tpc_noise", "tpc_noise");
 		short tick;
