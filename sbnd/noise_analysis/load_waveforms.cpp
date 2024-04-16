599db07732ccede1b706ca3ea934ded35613ba04
Dan Carber <whasian63@gmail.com>
2024-04-16 09:19:16 -0500

    Grabbing event info

--- sbnd/noise_analysis/load_waveforms.cpp
+++ sbnd/noise_analysis/load_waveforms.cpp
@@ -46,6 +46,7 @@ void LoadRawDigits(TFile *inFile,int sel_evt)
 	TTreeReader Events("Events;1", inFile);
 	//Events.Print();
 	TString filename = "waveform_"+TString(sel_evt)+".root";
+	TTreeReaderArray<int> event_info(Events, "EventAuxiliary.id_.event_");
 	TTreeReaderArray<raw::RawDigit> myADC(Events, "raw::RawDigits_daq__TPCDECODER.obj");
 	TFile* file = new TFile(, "RECREATE");
 	TTree* tree = new TTree("tpc_noise", "tpc_noise");
