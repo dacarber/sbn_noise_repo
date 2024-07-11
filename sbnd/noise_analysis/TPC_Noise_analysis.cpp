3371142a9a239652b422a4f404bbdea638b3a75b
Dan Carber <whasian63@gmail.com>
2024-07-11 12:02:05 -0600

    Added prin statements

--- sbnd/noise_analysis/TPC_Noise_analysis.cpp
+++ sbnd/noise_analysis/TPC_Noise_analysis.cpp
@@ -41,7 +41,9 @@ vector<float> Hit_removal(vector<float> channel,float Pedestal){
 	float pedestal = Pedestal;
 	vector<float> noise;
 	for (int j = 0; j < channel.size();j++){
+		cout<<"ADC with pedestal: "<<ADC<<endl;
 		float ADC = (float) TMath::Abs(ADCs.at(j)-pedestal);
+		cout<<"ADC without pedestal: "<<ADC<<endl;
 		if (ADC > 10.0){
 			//noise.push_back(ADC);
 			continue;
