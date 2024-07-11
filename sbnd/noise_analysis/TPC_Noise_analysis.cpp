243fb191189b81b9b504f47b34b83fe09a2b432c
Dan Carber <whasian63@gmail.com>
2024-07-11 11:09:20 -0600

    Change mean calculation

--- sbnd/noise_analysis/TPC_Noise_analysis.cpp
+++ sbnd/noise_analysis/TPC_Noise_analysis.cpp
@@ -36,27 +36,21 @@
 using namespace std;
 
 vector<float> Hit_removal(vector<float> channel,float Pedestal){	
-	vector<short> noise_channels;
-	//for (int i = 0; i <=channels.GetSize();i++){
-	//cout<<"Start of Hit Removal"<<endl;
-		vector<float> ADCs = channel;
+	vector<float> ADCs = channel;
 
-		float pedestal = Pedestal;
-		vector<float> noise;
-		for (int j = 0; j < channel.size();j++){
-			//cout<<"Start of searching for hits"<<endl;
-			float ADC = abs(ADCs.at(j)-pedestal);
-			if (ADC > 10.0){
-				//noise.push_back(ADC);
-				continue;
-			}
-			else{
-				noise.push_back(ADC);
-			}
-		
+	float pedestal = Pedestal;
+	vector<float> noise;
+	for (int j = 0; j < channel.size();j++){
+		float ADC = abs(ADCs.at(j)-pedestal);
+		if (ADC > 10.0){
+			//noise.push_back(ADC);
+			continue;
 		}
-		//noise_channels.push_back(noise);
-	//}
+		else{
+			noise.push_back(ADC);
+		}
+	
+	}
 	return noise;
 
 }
@@ -65,7 +59,8 @@ float Noise_levels(vector<float> noise_channels){
 	float RMS;
 	float square;
 	float sum;
-	float mean = accumulate(noise_channels.begin(),noise_channels.end(),0.0f)/noise_channels.size();
+	float mean =TMath::Mean(noise_channels.begin(),noise_channels.end());
+	//float mean = accumulate(noise_channels.begin(),noise_channels.end(),0.0f)/noise_channels.size();
 	for (int i = 0; i<noise_channels.size();i++){
 		square = (noise_channels[i]-mean) * (noise_channels[i]-mean);
 		sum = sum+square;
@@ -88,7 +83,7 @@ void LoadRawDigits(TFile *inFile)
 	TTreeReaderArray<raw::RawDigit> myADC(Events, "raw::RawDigits_daq__TPCDECODER.obj");
 	vector<float> RMS_total(11264,0.0f);
 	cout<<"Running Events"<<endl;
-	int evt = 0;
+	float evt = 0.0;
 	while (Events.Next())
 	{
 		
@@ -127,22 +122,16 @@ void LoadRawDigits(TFile *inFile)
 			cout<<"RMS:"<<RMS<<endl;
 			RMS_total[ki] =  RMS_total.at(ki)+RMS;
 		}
-		evt+=1;
+		evt+=1.0;
 		cout<<"Event:"<<evt<<endl;
-		//break;
 	}
 	
 	TFile* file = new TFile("noise_output.root", "RECREATE");
 	TTree* tree = new TTree("tpc_noise", "tpc_noise");
 	float avg_rms;
-	//vector<float> avg_FFT;
 	tree->Branch("raw_rms", &avg_rms, "avg_rms/F");
-	//tree->Branch("avg_FFT", &avg_FFT, "avg_FFT/F");
 	for(int ch = 0; ch<RMS_total.size(); ch++){
 		avg_rms = RMS_total.at(ch)/evt;
-		//for (size_t c = 0; c < FFT_total[ch].size(); ++c) {
-                //	avg_FFT[c] = FFT_total[ch][c]/evt;
-                //}
 		tree->Fill();	
 	}
 	
@@ -150,27 +139,6 @@ void LoadRawDigits(TFile *inFile)
 	file->Close();
 	
 	cout<<"Got ADC and Pedestal"<<endl;
-
-	/*TBranch* RawDigits = Events->GetBranch("raw::RawDigits_daq__DetSim.obj");
-	cout<<"Got Branch"<<endl;
-	TLeaf* ADC = RawDigits->GetLeaf("fADC");
-	int entries = Events->GetEntries("EventAuxiliary.id_.event_");
-
-	cout<<entries<<endl;
-	for (int i=0;i<entries;i++){
-		cout<<RawDigits->GetRow(i)<<endl;
-		cout<<ADC->GetValue(i)<<endl;
-	}*/
-	//TBranch* RawDigits = RawDigits_branch->GetSubBranch('raw::RawDigits_daq__DetSim.obj');
-
-
-	//Double_t ADC = RawDigits->GetLeaf("fADC")->GetValue(1);
-	
-
-	//cout<<ADC<<endl;
-
-	//TH1F* hist = new TH1F('fADC');
-	//Events->Draw('raw::RawDigits_daq__DetSim.obj.fADC');
 
 }
 
@@ -182,27 +150,4 @@ void TPC_Noise_analysis(TString inputFile="/exp/sbnd/data/users/trj/run11505/tpc
 	cout<<"Got File"<<endl;
 	LoadRawDigits(inFile);
 }
-/*void Hit_removal(auto channels)
-{
-	for (int i = 0; i <=channels.GetSize();i++){
-		vector<short> noise_channels;
-		int sum = accumulate(channels[i].begin(), channels[i].end(), 0);
-  		double mean = double(sum) / channels[i].size();
-		cout<<"Length of channel and mean before:"+channels[i].size()+mean<<endl;
-		for (int j = 0; j =< 34;j++){
-			double max_sig = *max_element(channels[i][j*100:(j+1)*100]);
-			double min_sig = *min_element(channels[i][j*100:(j+1)*100]);
-			double sig_diff = max_sig-min_sig;
-			if sig_diff > 30{
-				noise_channels.insert(i*100,vector<short> zeros(100,0.0));
-			noise_channels.insert(i*100,channels[i][j*100:(j+1)*100]);
-			}
-		}
-		int sum = accumulate(noise_channels.begin(), noise_channels.end(), 0);
-  		mean = double(sum) / noise_channels.size();
-		channels[i] = noise_channels;
-		cout<<"Length of channel and mean after:"+noise_channels[i].size()+mean<<endl;
-	}
-	cout<<"Number of channels"+noise_channels[i].size()<<endl;
 
-}*/
