a1dc550c96b5eee1819748de27b3f747f738f08e
Dan Carber <whasian63@gmail.com>
2024-07-11 12:03:06 -0600

    Type

--- sbnd/noise_analysis/TPC_Noise_analysis.cpp
+++ sbnd/noise_analysis/TPC_Noise_analysis.cpp
@@ -41,7 +41,7 @@ vector<float> Hit_removal(vector<float> channel,float Pedestal){
 	float pedestal = Pedestal;
 	vector<float> noise;
 	for (int j = 0; j < channel.size();j++){
-		cout<<"ADC with pedestal: "<<ADC<<endl;
+		cout<<"ADC with pedestal: "<<ADCs.at(j)<<endl;
 		float ADC = (float) TMath::Abs(ADCs.at(j)-pedestal);
 		cout<<"ADC without pedestal: "<<ADC<<endl;
 		if (ADC > 10.0){
