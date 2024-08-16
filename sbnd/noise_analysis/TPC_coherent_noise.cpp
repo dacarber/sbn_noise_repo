8dab2b992e0b7951b06e030809430a84c920a4e2
Dan Carber <whasian63@gmail.com>
2024-08-16 13:39:34 -0600

    Changed thresholds

--- sbnd/noise_analysis/TPC_coherent_noise.cpp
+++ sbnd/noise_analysis/TPC_coherent_noise.cpp
@@ -139,7 +139,7 @@ void LoadRawDigits(TFile *inFile)
 			bool skip_channel = false;
 			vector<short> x(myADC[index].Samples(),0);
 			for (size_t itick=0; itick < myADC[index].Samples(); ++itick){ 
-				if (abs(myADC[index].ADC(itick)-myADC[index].GetPedestal()) >  10){
+				if (abs(myADC[index].ADC(itick)-myADC[index].GetPedestal()) >  20){
 					skip_channel = true;
 					break;
 				}
