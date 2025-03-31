225b07cf3657022f560fb2c8fea7214f1e0b7e54
Dan Carber <whasian63@gmail.com>
2025-03-31 15:09:31 -0600

    Bug fix

--- sbnd/noise_analysis/TPC_RIC_WC_nosie_analysis.cpp
+++ sbnd/noise_analysis/TPC_RIC_WC_nosie_analysis.cpp
@@ -178,8 +178,7 @@ void LoadRawDigits(TFile *inFile)
         		continue;
         	}
         	string name = "h"+to_string(hist_name[1])+"_raw"+to_string(hist_name.back())+";"+to_string((numKeys/12)-1-event);
-        	TObject* obj_raw = inFile->Get(name);
-        	TH2D* hist2D_raw = (TH2D*)key->ReadObj();
+        	TH2D* hist2D_raw = (TH2D*)inFile->Get(name);
     		std::cout << "Found Histogram: " << hist_name
             << ", Entries: " << hist2D->GetNbinsX() << std::endl;
             std::cout << "Key Name: " << key->GetName() << std::endl;
@@ -272,7 +271,6 @@ void LoadRawDigits(TFile *inFile)
         delete hist2D;
         delete obj;
         delete hist2D_raw;
-        delete obj_raw;
 	}
 	
 	TFile* file = new TFile("noise_output_fft.root", "RECREATE");
